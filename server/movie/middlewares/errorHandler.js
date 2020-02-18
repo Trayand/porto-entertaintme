480d25a2965aa14c90216ea63ad3c7242615c44b
HBJKT033 <patradyn@gmail.com>
2020-02-18 19:23:18 +0700

    error handler done

--- server/movie/middlewares/errorHandler.js
+++ server/movie/middlewares/errorHandler.js
@@ -1,7 +1,29 @@
 module.exports = function (error, req, res, next) {
     console.log(error)
 
-    res.status(500).json({
-        message: 'Internal Server Error'
+    let statusCode,
+        messageError = []
+
+    switch (error.name) {
+        case "ValidationError":
+            statusCode = 400;
+            for (const key in error.errors) {
+                messageError.push(error.errors[key].message);
+            };
+            break;
+        case 'CastError':
+            statusCode = 400
+            messageError = "passed id invalid"
+            break;
+        default:
+            statusCode = error.status || 500;
+            messageError = error.msg || "Internal Server Error";
+            break;
+    }
+
+
+    res.status(statusCode).json({
+        message: messageError
     })
+
 }
